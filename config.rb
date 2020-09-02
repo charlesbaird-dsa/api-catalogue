a4b1efd8d4f8e6539a5fc8d83d23b2818017ed50
charlesbaird-dsa <charles.baird@digital.cabinet-office.gov.uk>
2020-09-02 20:34:28 +0100

    http prefix

--- config.rb
+++ config.rb
@@ -2,6 +2,9 @@ require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
 
+#deal with the folder structure
+set :http_prefix, '/api-catalogue'
+
 # use relative paths for links and sources
 activate :relative_assets
 set :relative_links, true 
